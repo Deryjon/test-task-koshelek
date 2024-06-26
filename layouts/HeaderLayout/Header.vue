cb03d43d79a504887b4538409ec98952f2bc5931
Dery Jon <yusupoviskandar1005@gmail.com>
2024-06-26 20:38:15 +0500

    start: started test task (2024-06-26, 20:38)

---
+++ layouts/HeaderLayout/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HeaderLogo from './components/HeaderLogo.vue'
+</script>
+<template>
+ <Header>
+     <div class="container">
+<HeaderLogo/>
+     </div>
+ </Header>
+</template>
